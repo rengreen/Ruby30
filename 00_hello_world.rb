a7d094841be1eb67e8e5353a15c26620a688ba3b
rengreen <r.m.gardzinska@gmail.com>
2018-11-07 18:51:49 +0100

    Day 0: Hello, world

---
+++ 00_hello_world.rb
@@ -0,0 +1,19 @@
+# 30 Days of Code. Day 0: Hello, World
+
+=begin
+Sample Input:
+Welcome to 30 Days of Code!
+
+Sample Output:
+Hello, World. 
+Welcome to 30 Days of Code!
+=end
+
+puts "Sample Input:"
+input_string = gets
+
+
+puts "\nSample Output:"
+puts 'Hello, World.'
+
+puts input_string
