0e9e036f2c77640d2b2cd24a56dd04b31616c2de
rengreen <r.m.gardzinska@gmail.com>
2018-11-07 18:54:10 +0100

    Day 1: Data Types

---
+++ 01_data_types.rb
@@ -0,0 +1,27 @@
+# 30 Days of Code. Day 1: Data Types
+
+=begin
+Sample Input:
+12
+4.0
+is the best place to learn and practice coding!
+
+Sample Output:
+16
+8.0
+HackerRank is the best place to learn and practice coding!
+=end
+
+i = 4
+d = 4.0
+s = "HackerRank "
+
+puts "Sample Input:"
+i1 = gets.to_i
+d1 = gets.to_f
+s1 = gets
+
+puts "\nSample Output:"
+puts i+i1
+puts d+d1
+puts s+s1
